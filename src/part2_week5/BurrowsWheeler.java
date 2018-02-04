81d4535d5ac24fbe27680f58b1b1f9abf14e1242
jasonkent27 <nancyyihao@qq.com>
2018-02-04 20:26:31 +0800

    add part2_week5 API

---
+++ src/part2_week5/BurrowsWheeler.java
@@ -0,0 +1,20 @@
+package part2_week5;
+
+public class BurrowsWheeler {
+
+    // apply Burrows-Wheeler transform, reading from standard input and writing to standard output
+    public static void transform() {
+
+    }
+
+    // apply Burrows-Wheeler inverse transform, reading from standard input and writing to standard output
+    public static void inverseTransform() {
+
+    }
+
+    // if args[0] is '-', apply Burrows-Wheeler transform
+    // if args[0] is '+', apply Burrows-Wheeler inverse transform
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/part2_week5/CircularSuffixArray.java
@@ -0,0 +1,24 @@
+package part2_week5;
+
+public class CircularSuffixArray {
+
+    // circular suffix array of s
+    public CircularSuffixArray(String s) {
+
+    }
+
+    // length of s
+    public int length() {
+        return 0;
+    }
+
+    // returns index of ith sorted suffix
+    public int index(int i) {
+        return 0;
+    }
+
+    // unit testing (required)
+    public static void main(String[] args) {
+
+    }
+}
